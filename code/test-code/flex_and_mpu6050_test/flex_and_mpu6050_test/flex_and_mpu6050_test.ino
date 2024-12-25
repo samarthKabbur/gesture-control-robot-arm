9fb73c82c1987e11f25451dfccb16445321420fd
Samarth Kabbur <samarthkabbur2016@gmail.com>
2024-12-25 13:38:01 -0600

    Added comments and combined test

---
+++ code/test-code/flex_and_mpu6050_test/flex_and_mpu6050_test/flex_and_mpu6050_test.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+
+#define button 8
+
+// Init variables
+int state = 0;
+int buttonState = 0;
+int check = 1;
+
+// define MPU addresses
+const int MPU1 = 0x68, MPU2 = 0x69
+
+// Store accel, temp, and gyro data
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- code/test-code/mpu6050-degrees-test/mpu6050-degrees-test.ino
+++ code/test-code/mpu6050-degrees-test/mpu6050-degrees-test.ino
@@ -3,112 +3,98 @@
   YouTube: https://www.youtube.com/channel/UCGxwyXJWEarxh2XWqvygiIg
   Fork your own version: https://github.com/EbenKouao/arduino-robotic-arm
   Date: 28/12/2020
-  Description: Test the MPU relative position X & Y co-ord in degreed
-
+  Description: Test the MPU relative position X & Y coordinates in degrees
 ******************************************************************************/
 
-#include<Wire.h>
+#include <Wire.h> // Include the Wire library for I2C communication
 
-#define button 8
+#define button 8 // Define the button pin
+
+// Initialize variables for button and system state
 int state = 0;
 int buttonState = 0;
 int check = 1;
 int nil = 0;
 
+// Define MPU addresses
 const int MPU2 = 0x69, MPU1 = 0x68;
 
+// Variables to store accelerometer, temperature, and gyroscope data
 int16_t AcX1, AcY1, AcZ1, Tmp1, GyX1, GyY1, GyZ1;
 
-int minVal = 0;
-int maxVal = 365;
+int minVal = 0; // Minimum value for sensor calibration
+int maxVal = 365; // Maximum value for sensor calibration
+double x, y, z; // Variables to store calculated angles
 
-// double x1;
-// double y1;
-// double z1;
-
-// double x2;
-// double y2;
-// double z2;
-
-double x, y, z;
-
-int response_time = 40;
+int response_time = 40; // Response time for the loop delay
 
 void setup() {
-  pinMode(3, OUTPUT);
-  Wire.begin();
+  pinMode(3, OUTPUT); // Set pin 3 as an output
+  
+  Wire.begin(); // Initialize the I2C communication
+
+  // Wake up MPU1
   Wire.beginTransmission(MPU1);
-  Wire.write(0x6B);// PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true); Wire.begin();
-  Wire.beginTransmission(MPU2);
-  Wire.write(0x6B);// PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B); // Write to the PWR_MGMT_1 register
+  Wire.write(0); // Set to zero to wake up the MPU-6050
   Wire.endTransmission(true);
-  Serial.begin(19200);
-  delay(1000);
-  // x1 = 0;
-  // y1 = 0;
-  // z1 = 0;
-  // x2 = 0;
-  // y2 = 0;
-  // z2 = 0;
 
+  // Wake up MPU2
+  Wire.begin();
+  Wire.beginTransmission(MPU2);
+  Wire.write(0x6B); // Write to the PWR_MGMT_1 register
+  Wire.write(0); // Set to zero to wake up the MPU-6050
+  Wire.endTransmission(true);
+
+  Serial.begin(19200); // Begin serial communication at 19200 baud
+  delay(1000); // Delay for stabilization
 }
+
 void loop() {
-  //get values for first mpu having address of 0x68
-  /*x1, y1, z1 = */ GetMpuValue1(MPU1/*, x1, y1, z1*/);
+  // Get values from the first MPU (address 0x68)
+  GetMpuValue1(MPU1);
   Serial.print("/");
-  /*x2, y2, z2 = */ GetMpuValue1(MPU2/*, x2, y2, z2*/);
-  Serial.println();
-  delay(50);
+
+  // Get values from the second MPU (address 0x69)
+  GetMpuValue1(MPU2);
+  Serial.println(); // Print a newline
+  delay(50); // Wait 50ms before the next loop iteration
 }
-                               /*, double x, double y, double z*/
+
+// Function to retrieve MPU data and calculate angles
 void GetMpuValue1(const int MPU) {
-
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.beginTransmission(MPU); // Start communication with the MPU
+  Wire.write(0x3B); // Start reading at register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 14, true); // request a total of 14 registers and place in the i2c access buffer
+  Wire.requestFrom(MPU, 14, true); // Request 14 registers (accelerometer, temp, and gyroscope data)
 
-  AcX1 = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AcY1 = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ1 = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  // Read accelerometer data
+  AcX1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for ACCEL_XOUT
+  AcY1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for ACCEL_YOUT
+  AcZ1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for ACCEL_ZOUT
 
-  // Tmp needs to be read because of how data is strctured in the i2c access buffer
-  Tmp1 = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  // Read temperature data (must be read to continue to gyro data)
+  Tmp1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for TEMP_OUT
 
+  // Map accelerometer data to angle ranges
   int xAng = map(AcX1, minVal, maxVal, -90, 90);
   int yAng = map(AcY1, minVal, maxVal, -90, 90);
   int zAng = map(AcZ1, minVal, maxVal, -90, 90);
 
-  GyX1 = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY1 = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ1 = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  // Read gyroscope data
+  GyX1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for GYRO_XOUT
+  GyY1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for GYRO_YOUT
+  GyZ1 = Wire.read() << 8 | Wire.read(); // Combine high and low bytes for GYRO_ZOUT
 
-  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI) + 4; //offset by 4 degrees to get back to zero
+  // Calculate angles using accelerometer data
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI) + 4; // Offset by 4 degrees to calibrate
   y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
   z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
 
-  // x += AcX1;
-  // y += AcY1;
-  // z += AcZ1;
-
-
-  //Serial.print("AngleX= ");
+  // Print calculated angles
   Serial.print(x);
   Serial.print("/");
-  //Serial.print("\t");
-
-  //Serial.print("AngleY= ");
   Serial.print(y);
   Serial.print("/");
-  //Serial.print("\t");
-
-  //Serial.print("AngleZ= ");
-  Serial.print(z); 
-  //Serial.print("\t");
-  //Serial.println("-----------------------------------------");
-
-  // return x, y, z;
+  Serial.print(z);
 }
