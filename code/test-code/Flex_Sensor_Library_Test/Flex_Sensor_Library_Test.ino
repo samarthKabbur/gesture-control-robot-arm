6060116461c927f48dd80981f3a746dca9a484b1
Samarth Kabbur <samarthkabbur2016@gmail.com>
2024-12-27 20:12:47 -0600

    Updated flex lib test code - library works

--- code/test-code/Flex_Sensor_Library_Test/Flex_Sensor_Library_Test.ino
+++ code/test-code/Flex_Sensor_Library_Test/Flex_Sensor_Library_Test.ino
@@ -1,20 +1,41 @@
 #include "flex_sensor.h"
 
-const int flexPin = A1;
+const int thumbPin = A1;
+const int pointerPin = A2;
+const int middlePin = A3;
 
 Flex_Sensor thumb;
 Flex_Sensor pointer;
+Flex_Sensor middle;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(4800);
+
   thumb.initialize();
   thumb.setThreshold(100);
-  thumb.set_flexPin(flexPin);
+  thumb.set_flexPin(thumbPin);
+
+  pointer.initialize();
+  pointer.setThreshold(100);
+  pointer.set_flexPin(pointerPin);
+
+  middle.initialize();
+  middle.setThreshold(100);
+  middle.set_flexPin(middlePin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Thumb: ")
-  thumb.printFingerState();
+  if (thumb.isBent()){
+    Serial.println("Thumb bent");
+  }
+  if (pointer.isBent()){
+    Serial.println("pointer bent");
+  }
+  if (middle.isBent()){
+    Serial.println("middle bent");
+  }
+
+
   delay(50);
 }
